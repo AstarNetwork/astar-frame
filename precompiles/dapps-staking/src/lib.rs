e14b8061ca64fdf8bfffa743a172fead9bbb271d
Dinonard <dino.pacandi@gmail.com>
2022-03-10 10:57:03 +0100

    Fix precompile compilation issue

--- precompiles/dapps-staking/src/lib.rs
+++ precompiles/dapps-staking/src/lib.rs
@@ -131,7 +131,7 @@ where
 
         // call pallet-dapps-staking
         let staking_info =
-            pallet_dapps_staking::Pallet::<R>::contract_stake_info(&contract_id, current_era)
+            pallet_dapps_staking::Pallet::<R>::contract_era_stake(&contract_id, current_era)
                 .unwrap_or_default();
         let gas_used = R::GasWeightMapping::weight_to_gas(R::DbWeight::get().read);
         // encode output with total
