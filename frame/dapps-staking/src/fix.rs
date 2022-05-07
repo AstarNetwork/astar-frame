3a5ba00dbb9996ae250d36e6b4bdeee1d3ccf6ed
codespool <4625220+codespool@users.noreply.github.com>
2022-05-07 22:00:20 +0300

    add logs and comments

--- frame/dapps-staking/src/fix.rs
+++ frame/dapps-staking/src/fix.rs
@@ -13,21 +13,25 @@ pub mod restake_fix {
     // Temp struct of corrected ContractStakeInfo records with progress data
     #[derive(Clone, PartialEq, Encode, Decode, RuntimeDebug, TypeInfo, Default)]
     pub struct RestakeFix<Balance: HasCompact> {
-        // store progress so iteration can continue in the next block
+        // store last processed staker from Ledger
         last_fully_processed_staker: Option<Vec<u8>>,
+        // store last processed staker's dapp
         last_processed_staker_contract: Option<Vec<u8>>,
+        // accumulate ContractStakeInfo by contract address
         contract_staking_info: BTreeMap<Vec<u8>, ContractStakeInfo<Balance>>,
-        // should be flipped once the process is complete
+        // flip to true when all Ledger data has been processed
         all_stakers_processed: bool,
     }
 
     pub fn restake_fix_migration<T: Config>(weight_limit: Weight) -> Weight {
+        log::info!("Executing a step of restake fix migration.");
         let mut restake_fix = RestakeFixAccumulator::<T>::get();
+        // one read for RestakeFixAccumulator
         let mut consumed_weight = T::DbWeight::get().reads(1);
-        // read all_stakers_processed
-        // if false:
+        // still stakers left to process
         if !restake_fix.all_stakers_processed {
-            // read ledger from last_processed_staker or first if None
+            log::info!("Accumulating staker info.");
+            // take iter from start or last processed
             let staker_iter = if let Some(last_processed_staker) =
                 restake_fix.last_fully_processed_staker.clone()
             {
@@ -35,10 +39,11 @@ pub mod restake_fix {
             } else {
                 Ledger::<T>::iter_keys()
             };
-            // for each record add to contract_staking info (amount and count)
-            // and add read weight
+            // star iterating, label to be able to break out from inner loop
             'staker_iter: for staker in staker_iter {
+                // every iteration adds one more read
                 consumed_weight = consumed_weight.saturating_add(T::DbWeight::get().reads(1));
+                // take iter for inner loop from start or last processed contract
                 let contract_iter = if let Some(last_processed_contract) =
                     restake_fix.last_processed_staker_contract.clone()
                 {
@@ -47,31 +52,48 @@ pub mod restake_fix {
                     GeneralStakerInfo::<T>::iter_prefix(&staker)
                 };
                 for (contract, staking_info) in contract_iter {
+                    log::info!(
+                        "Extracting contract stake data for staker address: {:?}.",
+                        staker
+                    );
+                    // every inner iteration also adds one read
                     consumed_weight = consumed_weight.saturating_add(T::DbWeight::get().reads(1));
                     let staked_value = staking_info.latest_staked_value();
+                    // get contract address as Vec<u8>
                     let contract_address = Ledger::<T>::storage_map_final_key(contract);
+                    // select staking_info for the contract
                     let mut contract_stake_info = restake_fix
                         .contract_staking_info
                         .entry(contract_address.clone())
                         .or_default();
 
+                    // update total and number of stakers
                     contract_stake_info.total =
                         contract_stake_info.total.saturating_add(staked_value);
-
                     contract_stake_info.number_of_stakers += 1;
+                    // check for total consumed weight
+                    // store last processed contract and break out if the limit is reached
                     if consumed_weight >= weight_limit {
+                        log::info!(
+                            ">>> Ledger migration stopped after consuming {:?} weight.",
+                            consumed_weight
+                        );
                         restake_fix.last_processed_staker_contract = Some(contract_address);
                         break 'staker_iter;
                     }
                 }
+
+                // when one staker is processed, store it and clear stored contract
                 restake_fix.last_processed_staker_contract = None;
                 restake_fix.last_fully_processed_staker =
                     Some(Ledger::<T>::storage_map_final_key(staker));
             }
 
+            // store updated accumulator and update total weight for that write
             RestakeFixAccumulator::<T>::put(restake_fix);
             consumed_weight = consumed_weight.saturating_add(T::DbWeight::get().writes(1));
         } else {
+            log::info!(">>> Starting to empty the accumulator");
             // if true
             // if contractStakeInfo is empty, we're done
             // for each ContractStakeInfo in RestakeFix
