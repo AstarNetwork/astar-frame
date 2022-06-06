09c4c23dd397eeb70d36ecba2b77495106cd3243
Alexander Krupenkin <mail@akru.me>
2022-06-06 18:39:28 +0300

    Added XCM.sol description

--- precompiles/xcm/XCM.sol
+++ precompiles/xcm/XCM.sol
@@ -6,7 +6,17 @@ pragma solidity ^0.8.0;
 interface XCM {
     /**
      * @dev Withdraw assets using PalletXCM call.
+     * @param asset_id - list of XC20 asset addresses
+     * @param asset_amount - list of transfer amounts (must match with asset addresses above)
+     * @param recipient_account_id - AccountId of destination account
+     * @param is_relay - set `true` for using relay chain as reserve
+     * @param parachain_id - set parachain id of reserve parachain (when is_relay set to false)
+     * @param fee_index - index of asset_id item that should be used as a XCM fee
      * @return A boolean confirming whether the XCM message sent.
+     *
+     * How method check that assets list is valid:
+     * - all assets resolved to multi-location (on runtime level)
+     * - all assets has corresponded amount (lenght of assets list matched to amount list)
      */
     function assets_withdraw(
         address[] asset_id,
