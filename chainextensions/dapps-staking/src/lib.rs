2f8219f4027630d5f3e55b9d56d0447a5762ff50
Maar-io <mario@stake.co.jp>
2022-07-04 09:50:06 +0200

    remove mmodule dependency from generic trait

--- chainextensions/dapps-staking/src/lib.rs
+++ chainextensions/dapps-staking/src/lib.rs
@@ -1,21 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use sp_runtime::DispatchError;
 
+use chain_extension_traits::ChainExtensionExec;
 use codec::Encode;
 use frame_support::log::trace;
 use pallet_contracts::chain_extension::{Environment, Ext, InitState, SysConfig, UncheckedFrom};
 use pallet_dapps_staking;
-
-pub trait AstarChainExtension {
-    fn execute_func<E, R>(
-        func_id: u32,
-        env: Environment<E, InitState>,
-    ) -> Result<(), DispatchError>
-    where
-        E: Ext,
-        <E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
-        R: pallet_dapps_staking::Config;
-}
 
 enum DappsStakingFunc {
     CurrentEra = 1,
@@ -39,19 +29,18 @@ impl TryFrom<u32> for DappsStakingFunc {
 }
 
 pub struct DappsStakingExtension;
-impl AstarChainExtension for DappsStakingExtension {
-    fn execute_func<E, R>(func_id: u32, env: Environment<E, InitState>) -> Result<(), DispatchError>
+impl<T: pallet_dapps_staking::Config> ChainExtensionExec<T> for DappsStakingExtension {
+    fn execute_func<E>(func_id: u32, env: Environment<E, InitState>) -> Result<(), DispatchError>
     where
-        E: Ext,
+        E: Ext<T = T>,
         <E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
-        R: pallet_dapps_staking::Config,
     {
         let func_id = DappsStakingFunc::try_from(func_id)?;
         let mut env = env.buf_in_buf_out();
         match func_id {
             // DappsStaking - current_era()
             DappsStakingFunc::CurrentEra => {
-                let current_era = pallet_dapps_staking::CurrentEra::<R>::get();
+                let current_era = pallet_dapps_staking::CurrentEra::<T>::get();
                 let current_era_encoded = current_era.encode();
                 trace!(
                     target: "runtime",
@@ -69,7 +58,7 @@ impl AstarChainExtension for DappsStakingExtension {
             // DappsStaking - general_era_info()
             DappsStakingFunc::GeneralEraInfo => {
                 let arg: u32 = env.read_as()?;
-                let era_info = pallet_dapps_staking::GeneralEraInfo::<R>::get(arg)
+                let era_info = pallet_dapps_staking::GeneralEraInfo::<T>::get(arg)
                     .ok_or(DispatchError::Other("general_era_info call failed"));
                 let era_info_encoded = era_info.encode();
                 trace!(
