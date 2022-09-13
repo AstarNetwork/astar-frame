666aa15e998e3dc080544466918d2d646d62d0f1
Dmitry Kashitsyn <korvin@deeptown.org>
2022-09-13 15:51:44 +0600

    Add reserve transfer API to the Solidity module

--- precompiles/xcm/XCM.sol
+++ precompiles/xcm/XCM.sol
@@ -68,4 +68,50 @@ interface XCM {
         bytes calldata call,
         uint64 transact_weight
     ) external returns (bool);
+
+    /**
+     * @dev Reserve transfer assets using PalletXCM call.
+     * @param asset_id - list of XC20 asset addresses
+     * @param asset_amount - list of transfer amounts (must match with asset addresses above)
+     * @param recipient_account_id - SS58 public key of the destination account
+     * @param is_relay - set `true` for using relay chain as reserve
+     * @param parachain_id - set parachain id of reserve parachain (when is_relay set to false)
+     * @param fee_index - index of asset_id item that should be used as a XCM fee
+     * @return A boolean confirming whether the XCM message sent.
+     *
+     * How method check that assets list is valid:
+     * - all assets resolved to multi-location (on runtime level)
+     * - all assets has corresponded amount (lenght of assets list matched to amount list)
+     */
+    function assets_reserve_transfer(
+        address[] calldata asset_id,
+        uint256[] calldata asset_amount,
+        bytes32   recipient_account_id,
+        bool      is_relay,
+        uint256   parachain_id,
+        uint256   fee_index
+    ) external returns (bool);
+
+    /**
+     * @dev Reserve transfer using PalletXCM call.
+     * @param asset_id - list of XC20 asset addresses
+     * @param asset_amount - list of transfer amounts (must match with asset addresses above)
+     * @param recipient_account_id - ETH address of the destination account
+     * @param is_relay - set `true` for using relay chain as reserve
+     * @param parachain_id - set parachain id of reserve parachain (when is_relay set to false)
+     * @param fee_index - index of asset_id item that should be used as a XCM fee
+     * @return A boolean confirming whether the XCM message sent.
+     *
+     * How method check that assets list is valid:
+     * - all assets resolved to multi-location (on runtime level)
+     * - all assets has corresponded amount (lenght of assets list matched to amount list)
+     */
+    function assets_reserve_transfer(
+        address[] calldata asset_id,
+        uint256[] calldata asset_amount,
+        address   recipient_account_id,
+        bool      is_relay,
+        uint256   parachain_id,
+        uint256   fee_index
+    ) external returns (bool);
 }
