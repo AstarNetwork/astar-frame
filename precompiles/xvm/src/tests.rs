05ccd04eb7e18acb273f4ee4eab3efb93376cb90
Dmitry Kashitsyn <korvin@deeptown.org>
2023-03-21 20:21:11 +0600

    Fix xvm_call test

--- precompiles/xvm/src/tests.rs
+++ precompiles/xvm/src/tests.rs
@@ -73,6 +73,12 @@ fn correct_arguments_works() {
                     .build(),
             )
             .expect_no_logs()
-            .execute_returns(EvmDataWriter::new().write(true).build());
+            .execute_returns(
+                EvmDataWriter::new()
+                    .write(false) // the XVM call should succeed but the internal should fail
+                    .write(1000000u64)
+                    .write(vec![0u8])
+                    .build(),
+            );
     })
 }
